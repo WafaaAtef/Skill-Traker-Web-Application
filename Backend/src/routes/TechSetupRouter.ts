a0b872813894a3018106a08b8441f1faec357d4c
yasmienalaa <yasmirnalaa167@gmail.com>
2026-09-16 23:25:39 +0300

    yarab

--- Backend/src/routes/TechSetupRouter.ts
+++ Backend/src/routes/TechSetupRouter.ts
@@ -1,7 +1,7 @@
 import {Router} from "express"
 import {addTrack,AddRoadMap,addSkills} from "../controllers/TechSetupController"
 import {AddTrackOrRoadmap} from "../middleware/techSetupMiddleware"
-import {auth} from "../middleware/authMidddleware"
+import {auth} from "../middleware/authMiddleware"
 const TechRouter =Router()
 
 
